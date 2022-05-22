0adbcc5117f1076eced11637dcee1b07ab985455
Daniel <gallegoruizdaniel@gmail.com>
2022-05-22 03:41:27 +0200

    Ejemplo de movimiento para el player

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class Movement : MonoBehaviour
+    {
+        public float speed = 1f;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Movement
+            var keyboardInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.Translate(keyboardInput * (speed * Time.deltaTime));
+        }
+    }
+}
